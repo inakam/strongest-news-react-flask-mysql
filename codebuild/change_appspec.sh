f2cba0980b2bba872b33f4f83334772999422a17
Ibuki Nakamura <nakamura.ibuki@nifty.co.jp>
2020-10-02 11:23:54 +0900

    CodePipeline周りの修正

--- codebuild/change_appspec.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-pwd
-ls
-TMP=$(aws ecs describe-task-definition --task-definition $1 | jq '.taskDefinition | .taskDefinitionArn ')
-TASK=$(echo $TMP | sed 's/\"//g')
-cat ./appspec.yaml | awk -v TASK=${TASK} '{ if (gsub(/_TASK_DEF_ARN/, TASK)){ print $0 > "./appspec.yaml" } else { print $0 > "./appspec.yaml" }  }'
-cat ./appspec.yaml
